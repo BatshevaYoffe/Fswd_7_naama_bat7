d18782dc61a33bf8045c6d3a7554b75932bc9374
BatshevaYoffe <73179986+BatshevaYoffe@users.noreply.github.com>
2023-07-25 12:37:24 +0300

    add book

---
+++ FSWD7/routes/book.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+
+const sqlPassword = "bat7Yoffe";
+
+function sqlConnect(query, values = []) {
+    return new Promise((resolve, reject) => {
+      const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: sqlPassword,
+        database: "library_fswd7",
+      });
+  
+      connection.connect((err) => {
+        if (err) {
+          console.error("Error connecting to MySQL server: " + err.stack);
+          reject(err);
+          return;
+        }
+        console.log("Connected to MySQL server");
+  
+        connection.query(query, values, (err, results) => {
+          if (err) {
+            console.error("Error executing query: " + err.code);
+            reject(err);
+          }
+  
+          connection.end((err) => {
+            if (err) {
+              console.error("Error closing connection: " + err.stack);
+              // reject(err);
+              return;
+            }
+            console.log("MySQL connection closed");
+          });
+  
+          resolve(results);
+        });
+      });
+    });
+  }
+    
+  router.post("/", function (req, res) {
+    const book = req.body;
+    const que = `SELECT * FROM users WHERE username = '${username}'`;
+    sqlConnect(que)
+      .then((result) => {
+        if (result.length > 0) {
+          console.log("you are exist");
+          return res.status(202);
+        }
+        const addUser = `INSERT INTO book ( book_name,author_name,publication_year) VALUES ('${book_name}', '${author_name}','${publication_year}' )`;
+        console.log(addUser);
+        sqlConnect(addUser)
+          .then((results) => {
+            const addToPass = `INSERT INTO password (username,password) VALUES ('${username}','${password}')`;
+            sqlConnect(addToPass) 
+            .then((result) => {
+              console.log("You are in the database");
+              return res.status(200);
+  
+            })
+            .catch((err) => {
+              console.error(err);
+              res.status(500).send("An error occurred");
+            });
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send("An error occurred");
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("An error occurred");
+      });
+});
+
+module.exports = router;
--- FSWD7/routes/connectTodb.js
+++ FSWD7/routes/connectTodb.js
@@ -40,3 +40,4 @@ function sqlConnect(query, values = []) {
     });
   }
   
+module.exports = sqlConnect;
